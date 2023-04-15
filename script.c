0f1aeb40734cd1175807548cdab39685f34399ee
musashi <gouhmada@gmail.com>
2023-04-15 02:17:20 +0000

    add clear

--- script.c
+++ script.c
@@ -7,17 +7,18 @@
  * + the ability to enter the name of commit incase you didn't the name would updated
 */
 
-int main ()
+int main (void)
 {   
-char command[3][1024] = {"git add .", "git commit -m 'updated'", "git push"}; // change here the name of the commit.
+char command[4][1024] = {"git add .", "git commit -m 'updated'", "git push", "clear"}; // change here the name of the commit.
 int loop = 3, i = 0;
 char input [1024];
 
-for (; i < loop; i++){
+for (; i < loop; i++)
+  {
 
    if (i == 1)
    {
-      printf("please enter the name of commit incase y didn't the cmt would be 'updated': ");
+      printf("enter the name commit if you don't "update" is your commit: ");
       scanf("%1024[^\n]s", input);
 
       if (strlen(input) > 0)
@@ -25,7 +26,6 @@ for (; i < loop; i++){
          sprintf(command[i], "git commit -m '%s' ", input);
       }
    }
-
    system(command[i]);
 }
    return 0;
