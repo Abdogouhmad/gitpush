6ab35e9a3d7b5fdccebe03e9c52cb3ca0911e0aa
div-styl <gouhmada@gmail.com>
2023-07-22 11:15:22 +0100

    updated

--- git.h
+++ git.h
@@ -6,7 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-/*prototypes*/
 #define MAX_COMMANDS 3
-
+/*prototypes*/
+void executeCommand(const char *command);
+void freeCommandsAndInputs(char **commands, int numCommands, char *input);
+char* getInput(void);
 #endif
---
+++ pushingfunc.c
@@ -0,0 +1,46 @@
+#include "git.h"
+/**
+ * executeCommand - execute a command
+ * @command: command to execute
+ * Return: void
+*/
+void executeCommand(const char *command)
+{
+    char *cmd = (char *)malloc(sizeof(char) * 100);
+    strcpy(cmd, command);
+    system(cmd);
+    free(cmd);
+}
+/**
+ * getInput - get the input from the user
+ * Return: the input
+*/
+char* getInput(void)
+{
+    char* input = NULL;
+    size_t size = 0;
+    
+    printf("Enter the commit name (or leave empty for 'update'): ");
+    getline(&input, &size, stdin); /*get the input using getline which allocate and free*/
+    
+    /*Remove trailing newline character*/
+    input[strcspn(input, "\n")] = '\0';
+    
+    return input;
+}
+/**
+ * freeCommandsAndInputs - free the commands and the input
+ * @commands: the commands
+ * @numCommands: the number of commands
+ * @input: the input
+ * Return: void
+*/
+void freeCommandsAndInputs(char **commands, int numCommands, char *input)
+{
+    int i = 0;
+    for (; i < numCommands; i++)
+    {
+        free(commands[i]);
+    }
+    free(input);
+}
