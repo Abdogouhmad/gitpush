6d054d849e57483332e4f4aea778b85dfc4d6d52
div-styl <gouhmada@gmail.com>
2023-08-14 21:00:39 +0100

    ignore tagret folder

--- push_in_rust/target/debug/build/memchr-6d8159d3a53145b9/build_script_build-6d8159d3a53145b9.d
+++
@@ -1,5 +0,0 @@
-/home/thorfinn/Desktop/gitpush/push_in_rust/target/debug/build/memchr-6d8159d3a53145b9/build_script_build-6d8159d3a53145b9: /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/memchr-2.5.0/build.rs
-
-/home/thorfinn/Desktop/gitpush/push_in_rust/target/debug/build/memchr-6d8159d3a53145b9/build_script_build-6d8159d3a53145b9.d: /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/memchr-2.5.0/build.rs
-
-/home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/memchr-2.5.0/build.rs:
--- push_in_rust/target/debug/deps/siphasher-a7c1b43b03f8bd5e.d
+++
@@ -1,9 +0,0 @@
-/home/thorfinn/Desktop/gitpush/push_in_rust/target/debug/deps/siphasher-a7c1b43b03f8bd5e.rmeta: /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/lib.rs /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip.rs /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip128.rs
-
-/home/thorfinn/Desktop/gitpush/push_in_rust/target/debug/deps/libsiphasher-a7c1b43b03f8bd5e.rlib: /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/lib.rs /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip.rs /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip128.rs
-
-/home/thorfinn/Desktop/gitpush/push_in_rust/target/debug/deps/siphasher-a7c1b43b03f8bd5e.d: /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/lib.rs /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip.rs /home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip128.rs
-
-/home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/lib.rs:
-/home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip.rs:
-/home/thorfinn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/siphasher-0.3.10/src/sip128.rs:
